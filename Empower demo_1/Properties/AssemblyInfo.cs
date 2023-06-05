f20f4db63533d465d1fac0f3b2233c7216eb1205
Stevenlim123 <steven_lim@broadcastpro.com.sg>
2023-06-05 15:11:35 +0800

    Add project files.

---
+++ Empower demo_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower demo_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower demo_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D8B31C52-776B-4C97-931E-13E7768C127E")]
+[assembly: AssemblyVersion("1.0.0.0")]
